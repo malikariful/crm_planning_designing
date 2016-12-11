88a39a025261b7efc3d3c9cab4bd379911989f01
Muhammad. Nazmul Ahsan <tarekahsan709@gmail.com>
2016-12-12 02:01:39 +0600

    initial commit

---
+++ client/app/account/settings/index.js
@@ -0,0 +1,7 @@
+'use strict';
+
+import SettingsController from './settings.controller';
+
+export default angular.module('angularSeedApp.settings', [])
+  .controller('SettingsController', SettingsController)
+  .name;
