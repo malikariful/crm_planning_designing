cfe0d1decb475048790203edc5ac0013e9c9028a
Md. Nazmul ahsan <tarekahsan709@gmail.com>
2017-04-09 17:55:05 +0600

    add user management user create

--- client/app/admin/userManagement/userManagement.controller.js
+++ client/app/admin/userManagement/userManagement.controller.js
@@ -2,6 +2,7 @@
 
 export default class UserManagementController {
     /*@ngInject*/
+
     constructor(User, ModalService, Auth, $rootScope) {
         this.$rootScope = $rootScope;
         this.Auth = Auth;
@@ -31,8 +32,8 @@ export default class UserManagementController {
     createUser = function () {
         this.ModalService.showModal({
             templateUrl: 'createUser.html',
-            controller: ['$rootScope', '$scope', 'close', 'Auth', 'User',
-                function ($rootScope, $scope, close, Auth, User) {
+            controller: ['$rootScope', '$scope', 'close', 'Auth', 'User','$element',
+                function ($rootScope, $scope, close, Auth, User, $element) {
                     $scope.roles = [
                         {
                             "name": "admin"
@@ -47,45 +48,47 @@ export default class UserManagementController {
                             "name": "employee"
                         }
                     ];
-                    $scope.createUser = function () {
-                        return Auth.createUserFromManagement({
-                            name: $scope.user.name,
-                            firstName: $scope.user.firstName,
-                            lastName: $scope.user.lastName,
-                            email: $scope.user.email,
-                            role: $scope.user.role[0].name,
-                            password: '123456'
-                        })
-                            .then(() => {
-                                $rootScope.users = User.query();
-                                close(null, 500);
-
+                    $scope.createUser = function (editForm) {
+                        if(editForm.$valid){
+                            return Auth.createUserFromManagement({
+                                name: $scope.user.name,
+                                firstName: $scope.user.firstName,
+                                lastName: $scope.user.lastName,
+                                email: $scope.user.email,
+                                role: $scope.user.role[0].name,
+                                password: '123456'
                             })
-                            .catch(err => {
-                                err = err.data;
-                                this.errors = {};
-                                if (err.name) {
-                                    angular.forEach(err.fields, function (field, key) {
-                                        form[key].$setValidity('sequelize', false);
-                                        this.errors[key] = err.message;
-
-                                    }, this);
-                                }
-                                close(null, 500);
-                            });
-
-                    };
-                    $scope.close = function () {
-                        close(null, 500);
+                                .then(() => {
+                                    $rootScope.users = User.query();
+                                    $element.modal('hide');
+                                    close(null, 500);
+                                })
+                                .catch(err => {
+                                    err = err.data;
+                                    $scope.errors = {};
+                                    if (err.name) {
+                                        angular.forEach(err.fields, function (field, key) {
+                                            editForm[key].$setValidity('sequelize', false);
+                                            $scope.errors[key] = err.message;
+                                        }, this);
+                                    }
+                                    setTimeout(function(){
+                                        $element.modal('hide');
+                                        close(null, 200);
+                                    }, 3000);
+                                });
+                        }
                     };
                 }
             ]
-
         }).then(function (modal) {
             modal.element.modal();
             modal.close.then(function (result) {
             });
 
+        }).catch(function(error) {
+            console.log('error contains a detailed error message.');
+            console.log(error);
         });
 
     };
