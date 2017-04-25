400495acc313eac98c391aaa0e80f94fbdc769de
Md. Nazmul ahsan <tarekahsan709@gmail.com>
2017-04-25 12:29:06 +0600

    show dealer in modal

--- client/app/dealer/dealer.controller.js
+++ client/app/dealer/dealer.controller.js
@@ -2,10 +2,32 @@
 
 export default class DealerController {
     /*@ngInject*/
-    constructor($scope, dealerService, $mdDialog) {
+    constructor($scope, dealerService, $mdDialog, ModalService) {
         this.dealers = dealerService.query();
         this.$mdDialog = $mdDialog;
         this.$scope = $scope;
+        this.ModalService = ModalService;
+
+
+
+        this.user = {
+            title: 'Developer',
+            email: 'ipsum@lorem.com',
+            firstName: '',
+            lastName: '',
+            company: 'Google',
+            address: '1600 Amphitheatre Pkwy',
+            city: 'Mountain View',
+            state: 'CA',
+            biography: 'Loves kittens, snowboarding, and can type at 130 WPM.\n\nAnd rumor has it she bouldered up Castle Craig!',
+            postalCode: '94043'
+        };
+
+        this.states = ('AL AK AZ AR CA CO CT DE FL GA HI ID IL IN IA KS KY LA ME MD MA MI MN MS ' +
+        'MO MT NE NV NH NJ NM NY NC ND OH OK OR PA RI SC SD TN TX UT VT VA WA WV WI ' +
+        'WY').split(' ').map(function(state) {
+            return {abbrev: state};
+        });
     }
 
     removeItem(dealer) {
@@ -13,7 +35,7 @@ export default class DealerController {
         this.dealers.splice(this.dealers.indexOf(dealer), 1);
     };
 
-    showConfirm = function (ev, dealer) {
+    deleteDealer = function (ev, dealer) {
         var confirm = this.$mdDialog.confirm()
             .title('Would you like to delete the dealer?')
             .textContent('All of the information will be gone.')
@@ -28,4 +50,68 @@ export default class DealerController {
         });
     };
 
+    showDealer = function (dealer) {
+        this.ModalService.showModal({
+            templateUrl: 'showDealer.html',
+            controller: ['$scope', 'dealer', function ($scope, dealer) {
+                console.log('Inside controller show Dealer');
+                console.log(dealer);
+                $scope.dealer = dealer;
+            }],
+            inputs: {
+                dealer: dealer
+            }
+        }).then(function (modal) {
+            modal.element.modal();
+            modal.close.then(function (dealer) {
+            });
+        });
+    };
+
+    editDealer = function (dealer) {
+        console.log('edit dealer');
+        console.log(dealer);
+        this.ModalService.showModal({
+            templateUrl: 'editDealer.html',
+            controller: ['$scope', 'dealer', function ($scope, dealer) {
+                console.log('Inside controller show Dealer');
+                console.log(dealer);
+                $scope.dealer = dealer;
+            }],
+            inputs: {
+                dealer: dealer
+            }
+        }).then(function (modal) {
+            modal.element.modal();
+            modal.close.then(function (dealer) {
+            });
+        });
+    };
+
 }
+
+
+// createdAt
+//     :
+//     "2017-04-24T10:26:02.000Z"
+// dealer_address
+//     :
+//     "Badda"
+// dealer_email
+//     :
+//     "sajal@gmail.com"
+// dealer_name
+//     :
+//     "Sajal"
+// dealer_phone
+//     :
+//     1913500623
+// dealer_type
+//     :
+//     "permanent"
+// updatedAt
+//     :
+//     "2017-04-24T10:26:02.000Z"
+// _id
+//     :
+//     24
--- client/app/dealer/index.js
+++ client/app/dealer/index.js
@@ -5,7 +5,17 @@ import DealerController from './dealer.controller';
 import DealerSetUpController from './dealerSetUp.controller';
 
 export default angular.module('crmApp.dealer', [])
-  .config(routes)
-  .controller('DealerController', DealerController)
-  .controller('DealerSetUpController', DealerSetUpController)
-  .name;
+    .config(routes)
+    .config(['$mdThemingProvider', function ($mdThemingProvider) {
+
+        // Configure a dark theme with primary foreground yellow
+
+        $mdThemingProvider.theme('docs-dark', 'default')
+            .primaryPalette('yellow')
+            .dark();
+
+        
+    }])
+    .controller('DealerController', DealerController)
+    .controller('DealerSetUpController', DealerSetUpController)
+    .name;
