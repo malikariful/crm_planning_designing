3b7df23ffd190a66c5438afad56a1b8eed0ee3af
Md. Nazmul ahsan <tarekahsan709@gmail.com>
2017-07-19 17:55:00 +0600

    add select motorcycle on client

--- client/app/jobCard/index.js
+++ client/app/jobCard/index.js
@@ -10,25 +10,21 @@ export default angular.module('crmApp.jobCard', ['crmApp.auth', 'ui.router', 'sm
     .name;
 
 
-function JobCardSetUpController($scope, $filter, $timeout, $q, $log) {
+function JobCardSetUpController($scope, vehicleService, $filter, $timeout, $q, $log) {
     var self = this;
+    self.vehicles = vehicleService.query();
+    console.log(self.vehicles);
 
     self.simulateQuery = false;
     self.isDisabled = false;
 
-    self.states = loadAll();
     self.querySearch = querySearch;
     self.selectedItemChange = selectedItemChange;
     self.searchTextChange = searchTextChange;
 
-    self.newState = newState;
-
-    function newState(state) {
-        alert("Sorry! You'll need to create a Constitution for " + state + " first!");
-    }
 
     function querySearch(query) {
-        var results = query ? self.states.filter(createFilterFor(query)) : self.states,
+        var results = query ? self.vehicles.filter(createFilterFor(query)) : self.vehicles,
             deferred;
         if (self.simulateQuery) {
             deferred = $q.defer();
@@ -42,38 +38,21 @@ function JobCardSetUpController($scope, $filter, $timeout, $q, $log) {
     }
 
     function searchTextChange(text) {
-        $log.info('Text changed to ' + text);
+        // $log.info('Text changed to ' + text);
     }
 
-    function selectedItemChange(item) {
-        $log.info('Item changed to ' + JSON.stringify(item));
+    function selectedItemChange(selectedVehicle) {
+        $log.info(selectedVehicle);
     }
 
-    function loadAll() {
-        var allStates = 'Alabama, Alaska, Arizona, Arkansas, California, Colorado, Connecticut, Delaware,\
-              Florida, Georgia, Hawaii, Idaho, Illinois, Indiana, Iowa, Kansas, Kentucky, Louisiana,\
-              Maine, Maryland, Massachusetts, Michigan, Minnesota, Mississippi, Missouri, Montana,\
-              Nebraska, Nevada, New Hampshire, New Jersey, New Mexico, New York, North Carolina,\
-              North Dakota, Ohio, Oklahoma, Oregon, Pennsylvania, Rhode Island, South Carolina,\
-              South Dakota, Tennessee, Texas, Utah, Vermont, Virginia, Washington, West Virginia,\
-              Wisconsin, Wyoming';
-
-        return allStates.split(/, +/g).map(function (state) {
-            return {
-                value: state.toLowerCase(),
-                display: state
-            };
-        });
-    }
 
     function createFilterFor(query) {
-        var lowercaseQuery = angular.lowercase(query);
-
-        return function filterFn(state) {
-            return (state.value.indexOf(lowercaseQuery) === 0);
+        return function filterFn(vehicle) {
+            return (vehicle.Vehicle_model.vehicle_model_name.indexOf(query) === 0);
         };
 
     }
+
 
     var allGroups = [
         'one',
@@ -81,15 +60,15 @@ function JobCardSetUpController($scope, $filter, $timeout, $q, $log) {
         'three'
     ];
 
-    $scope.queryGroups = function(search) {
+    $scope.queryGroups = function (search) {
         var firstPass = $filter('filter')(allGroups, search);
 
-        return firstPass.filter(function(item) {
+        return firstPass.filter(function (item) {
             return $scope.selectedGroups.indexOf(item) === -1;
         });
     };
 
-    $scope.addGroup = function(group) {
+    $scope.addGroup = function (group) {
         $scope.selectedGroups.push(group);
     };
 
@@ -97,7 +76,7 @@ function JobCardSetUpController($scope, $filter, $timeout, $q, $log) {
 
     $scope.selectedGroups = [allGroups[0]];
 
-    $scope.$watchCollection('selectedGroups', function() {
+    $scope.$watchCollection('selectedGroups', function () {
         $scope.availableGroups = $scope.queryGroups('');
     });
 }
